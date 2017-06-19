e228bfc97c2cb06712c70ba85278b4a7ed3ddee4
Andrew Rycharski 18 <andrew.rycharski18@montville.net>
2017-06-19 11:50:50 -0400

    All the animations

--- main.ts
+++ main.ts
@@ -2,11 +2,27 @@ enum animationID {
     //% block="Intro-Spin-Outro-X"
     introOutro,
     //% block="Hashtag"
-    hashtag
+    hashtag,
+	//% block="Count Down"
+	countdown,
+	//% block="Mood Swing"
+	moodswing,
+	//% block="Diag-Fill Diag-Empty"
+	diagfillempt,
+	//% block="Diag Wipe"
+	diagwipe,
+	//% block="Looping Loading Circle"
+	looploadcirc,
+	//% block="Inside Out Swirl"
+	inoutswirl,
+	//% block="Outside In Swirl"
+	outinswirl,
+	//% block="Start-up Animation"
+	startup
 }
 
 //% color=#FF0000  block="Andrimations" icon="\uf2b0"
-namespace animations {
+namespace Andrimations {
     //% blockId=showAndrimation
     //% block="show andrimation %animation"
     export function showAndrewAnimation(animation: animationID): void {
@@ -113,6 +129,480 @@ namespace animations {
         `).scrollImage(5, 150)
                 })
                 break;
+		case animationID.countdown:
+			 images.createBigImage(`
+        # # # # # . . # # .
+        # . . . . . # . # .
+        # # # # . # . . # .
+        . . . . # # # # # #
+        # # # # . . . . # .
+        `).scrollImage(5, 800)
+    images.createBigImage(`
+        # # # # # # # # # .
+        . . . . # . . . . #
+        . . # # . . # # # .
+        # . . . # # . . . .
+        . # # # . # # # # #
+        `).scrollImage(5, 800)
+    images.createBigImage(`
+        . . # . . . # # # .
+        . # # . . # . . # #
+        . . # . . # . # . #
+        . . # . . # # . . #
+        . # # # . . # # # .
+        `).scrollImage(5, 800)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . # . # .
+        . . . . . . . . . .
+        . . . . . # . . . #
+        . . . . . . # # # .
+        `).scrollImage(5, 500)
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `).scrollImage(5, 800)
+			break;
+		case animationID.moodswing:
+		 images.createBigImage(`
+        . # . # . . # . # .
+        . . . . . . . . . .
+        # . . . # . . . . .
+        . # # # . # # # # #
+        . . . . . . . . . .
+        `).scrollImage(5, 1000)
+    images.createBigImage(`
+        . # . # . . . . . .
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . # # # . . . . . .
+        # . . . # . . . . .
+        `).scrollImage(5, 1000)
+		break;
+	case animationID.diagfillempt:
+	images.createBigImage(`
+    # . . . . # # . . .
+    . . . . . # . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 200)
+images.createBigImage(`
+    # # # . . # # # # .
+    # # . . . # # # . .
+    # . . . . # # . . .
+    . . . . . # . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 200)
+images.createBigImage(`
+    # # # # # # # # # #
+    # # # # . # # # # #
+    # # # . . # # # # .
+    # # . . . # # # . .
+    # . . . . # # . . .
+    `).scrollImage(5, 200)
+images.createBigImage(`
+    # # # # # # # # # #
+    # # # # # # # # # #
+    # # # # # # # # # #
+    # # # # . # # # # #
+    # # # . . # # # # .
+    `).scrollImage(5, 200)
+images.createBigImage(`
+    # # # # # . # # # #
+    # # # # # # # # # #
+    # # # # # # # # # #
+    # # # # # # # # # #
+    # # # # # # # # # #
+    `).scrollImage(5, 200)
+images.createBigImage(`
+    . . # # # . . . # #
+    . # # # # . . # # #
+    # # # # # . # # # #
+    # # # # # # # # # #
+    # # # # # # # # # #
+    `).scrollImage(5, 200)
+images.createBigImage(`
+    . . . # # . . . . #
+    . . # # # . . . # #
+    . # # # # . . # # #
+    # # # # # . # # # #
+    # # # # # # # # # #
+    `).scrollImage(5, 200)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . # . . . . .
+    . . . # # . . . . #
+    . . # # # . . . # #
+    . # # # # . . # # #
+    `).scrollImage(5, 200)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . # . . . . .
+    . . . # # . . . . #
+    `).scrollImage(5, 200)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 200)
+	break;
+case animationID.diagwipe:
+ images.createBigImage(`
+        # . . . . # # . . .
+        . . . . . # . . . .
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . . . . . .
+        `).scrollImage(5, 200)
+    images.createBigImage(`
+        # # # . . . # # # .
+        # # . . . # # # . .
+        # . . . . # # . . .
+        . . . . . # . . . .
+        . . . . . . . . . .
+        `).scrollImage(5, 200)
+    images.createBigImage(`
+        . . # # # . . . # #
+        . # # # . . . # # #
+        # # # . . . # # # .
+        # # . . . # # # . .
+        # . . . . # # . . .
+        `).scrollImage(5, 200)
+    images.createBigImage(`
+        . . . . # . . . . .
+        . . . # # . . . . #
+        . . # # # . . . # #
+        . # # # . . . # # #
+        # # # . . . # # # .
+        `).scrollImage(5, 200)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . # . . . . .
+        . . . # # . . . . #
+        . . # # # . . . # #
+        `).scrollImage(5, 200)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . # . . . . .
+        `).scrollImage(5, 200)
+		break;
+		case animationID.looploadcirc:
+		images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . # . . .
+    . # . . . . # . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 100)
+for (let i = 0; i < 4; i++) {
+    images.createBigImage(`
+        . . . . . . . . . .
+        . # # . . . # # # .
+        . # . . . . . . . .
+        . . . . . . . . . .
+        . . . . . . . . . .
+        `).scrollImage(5, 100)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . # # . . . . # .
+        . . . # . . . . # .
+        . . . . . . . . # .
+        . . . . . . . . . .
+        `).scrollImage(5, 100)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . # . . . . . .
+        . . # # . . # # # .
+        . . . . . . . . . .
+        `).scrollImage(5, 100)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . # . . .
+        . # . . . . # . . .
+        . # # . . . # . . .
+        . . . . . . . . . .
+        `).scrollImage(5, 100)
+}
+images.createBigImage(`
+    . . . . . . . . . .
+    . # # . . . # # # .
+    . # . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 100)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . # # . . . . # .
+    . . . # . . . . # .
+    . . . . . . . . # .
+    . . . . . . . . . .
+    `).scrollImage(5, 100)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . # . . . . . .
+    . . # # . . # # # .
+    . . . . . . . . . .
+    `).scrollImage(5, 100)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . # . . .
+    . # . . . . # . . .
+    . # # . . . # . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 100)
+images.createBigImage(`
+    . . . . . . . . . .
+    . # # . . . # # . .
+    . # . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 100)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . # . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 100)
+	break;
+		case animationID.inoutswirl:
+	images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . # . . . # # . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 100)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . # # . . . # . . .
+    . # . . . . # # . .
+    . . . . . . . . . .
+    `).scrollImage(5, 100)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . # .
+    . # # # . . . # # .
+    . . . . . . . . . .
+    `).scrollImage(5, 100)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . # . . . # # .
+    . . . # . . . . # .
+    . . . # . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 100)
+images.createBigImage(`
+    . . . . . . . . . .
+    . # # # . # # # . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 100)
+images.createBigImage(`
+    . . . . . . . . . .
+    # # . . . # . . . .
+    # . . . . # . . . .
+    . . . . . # . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 100)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . . . . .
+    # . . . . . . . . .
+    # . . . . # . . . .
+    # . . . . # # . . .
+    `).scrollImage(5, 100)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    # # # . . . # # # .
+    `).scrollImage(5, 100)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . #
+    . . # # # . . . # #
+    `).scrollImage(5, 100)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . . . . #
+    . . . . # . . . . #
+    . . . . # . . . . #
+    . . . . # . . . . .
+    `).scrollImage(5, 100)
+images.createBigImage(`
+    . . . . # . . . # #
+    . . . . # . . . . #
+    . . . . # . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 100)
+images.createBigImage(`
+    . . # # # . # # # .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 100)
+images.createBigImage(`
+    # # # . . # # . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 100)
+images.createBigImage(`
+    # . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 100)
+	break;
+	case animationID.outinswirl:
+	images.createBigImage(`
+    # . . . . # # . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 150)
+images.createBigImage(`
+    # # # . . . # # # .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 150)
+images.createBigImage(`
+    . . # # # . . . # #
+    . . . . . . . . . #
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 150)
+images.createBigImage(`
+    . . . . # . . . . .
+    . . . . # . . . . #
+    . . . . # . . . . #
+    . . . . . . . . . #
+    . . . . . . . . . .
+    `).scrollImage(5, 150)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . # . . . . .
+    . . . . # . . . . #
+    . . . . # . . . # #
+    `).scrollImage(5, 150)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . # # # . # # # .
+    `).scrollImage(5, 150)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . # . . . .
+    # # # . . # # . . .
+    `).scrollImage(5, 150)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . # . . . .
+    # . . . . # . . . .
+    # . . . . # . . . .
+    # . . . . . . . . .
+    `).scrollImage(5, 150)
+images.createBigImage(`
+    . . . . . . . . . .
+    # # . . . # # # . .
+    # . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 150)
+images.createBigImage(`
+    . . . . . . . . . .
+    . # # # . . . # # .
+    . . . . . . . . # .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 150)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . # . . . . . .
+    . . . # . . . . # .
+    . . . # . . . # # .
+    . . . . . . . . . .
+    `).scrollImage(5, 150)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . # . . .
+    . # # # . . # # . .
+    . . . . . . . . . .
+    `).scrollImage(5, 150)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . # # . . . # # . .
+    . # . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 150)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . # . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    `).scrollImage(5, 150)
+break;
+case animationID.startup:
+images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . # . .
+        . . # . . . # # # .
+        . . . . . . . # . .
+        . . . . . . . . . .
+        `).scrollImage(5, 60)
+    images.createBigImage(`
+        . . # . . . # # # .
+        . # # # . # # # # #
+        # # # # # # # # # #
+        . # # # . # # # # #
+        . . # . . . # # # .
+        `).scrollImage(5, 60)
+    images.createBigImage(`
+        # . . . # . . . . .
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . . . . . .
+        # . . . # . . . . .
+        `).scrollImage(5, 60)
+		break;
+
         }
     }
 }
